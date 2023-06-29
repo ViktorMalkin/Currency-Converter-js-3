f22d4effdf38a6572a6224050c2f4a000badc01e
Viktor <121763231+ViktorMalkin@users.noreply.github.com>
2023-06-29 11:57:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,13 +7,16 @@ getButton = document.querySelector('form button');
 
 for(let i = 0; i < dropList.length; i++) {
 	for( let currency_code in country_code) {
-		let selected;
-		if(i == 0){
-			selected = currency_code == "USD" ? "selected" : "";
-		}else if(i == 1) {
-			selected = currency_code == "PLN" ? "selected" : "";
-		}
+
+		//let selected;
+		//if(i == 0){
+			//selected = currency_code == "USD" ? "selected" : "";
+		//}else if(i == 1) {
+			//selected = currency_code == "PLN" ? "selected" : "";
+		//}
 		//console.log(currency_code)
+		
+		
 		let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
 
 		dropList[i].insertAdjacentHTML("beforeend", optionTag);
